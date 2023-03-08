bf18a1ee0f5c1a73dbf52c6de5f2377fd0e9313a
SoulKeeper <108797195+8soulkeeper8@users.noreply.github.com>
2023-03-08 20:24:17 +0300

    Update change currency
    Add change price for menu!

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ function buttonClick(elem) {
 
 // wallet change script
 const currencySelector = document.getElementById("currency");
-const item1 = document.getElementById("salecard1");
-const item2 = document.getElementById("salecard2");
-const item3 = document.getElementById("salecard3");
-const item4 = document.getElementById("salecard4");
+const item1 = document.getElementById("price1");
+const item2 = document.getElementById("price2");
+const item3 = document.getElementById("price3");
+const item4 = document.getElementById("price4");
+const item5 = document.getElementById("price5");
+const item6 = document.getElementById("price6");
+const item7 = document.getElementById("price7");
+const item8 = document.getElementById("price8");
+const item9 = document.getElementById("price9");
+const item10 = document.getElementById("price10");
+const item11 = document.getElementById("price11");
+const item12 = document.getElementById("price12");
+
 
 // сохраняем выбранную валюту в localStorage
 function handleCurrencyChange() {
@@ -16,25 +25,57 @@ function handleCurrencyChange() {
   localStorage.setItem('selectedCurrency', selectedCurrency);
 
   if (selectedCurrency === "USD") {
-    item1.innerHTML = '15$';
-    item2.innerHTML = '13$';
-    item3.innerHTML = '9$';
-    item4.innerHTML = '25$';
+    item1.innerHTML = 5 + "$";
+    item2.innerHTML = 1 + "$";
+    item3.innerHTML = 1.6 + "$";
+    item4.innerHTML = 2.4 + "$";
+    item5.innerHTML = 3.4 + "$";
+    item6.innerHTML = 4 + "$";
+    item7.innerHTML = 0.8 + "$";
+    item8.innerHTML = 4.6 + "$";
+    item9.innerHTML = 4.8 + "$";
+    item10.innerHTML = 1.4 + "$";
+    item11.innerHTML = 5.4 + "$";
+    item12.innerHTML = 1.2 + "$";
   } else if (selectedCurrency === "RUB") {
-    item1.innerHTML = 'Double Drive Burger <p><del>754 ₽</del> 641 ₽</p>';
-    item2.innerHTML = 'Chicago Classic <p><del>528 ₽</del> 377 ₽</p>';
-    item3.innerHTML = 'Hawaii Royal <p><del>339 ₽</del> 226 ₽</p>';
-    item4.innerHTML = '222';
+    item1.innerHTML = (5*75).toFixed(2) + "₽";
+    item2.innerHTML = (1*75).toFixed(2) + "₽";
+    item3.innerHTML = (1.6*75).toFixed(2) + "₽";
+    item4.innerHTML = (2.4*75).toFixed(2) + "₽";
+    item5.innerHTML = (3.4*75).toFixed(2) + "₽";
+    item6.innerHTML = (4*75).toFixed(2) + "₽";
+    item7.innerHTML = (0.8*75).toFixed(2) + "₽";
+    item8.innerHTML = (4.6*75).toFixed(2) + "₽";
+    item9.innerHTML = (4.8*75).toFixed(2) + "₽";
+    item10.innerHTML = (1.4*75).toFixed(2) + "₽";
+    item11.innerHTML = (5.4*75).toFixed(2) + "₽";
+    item12.innerHTML = (1.2*75).toFixed(2) + "₽";
   } else if (selectedCurrency === "EUR") {
-    item1.innerHTML = 'Double Drive Burger <p><del>9,4 €</del> 8 €</p>';
-    item2.innerHTML = 'Chicago Classic <p><del>6.6 €</del> 4,7 €</p>';
-    item3.innerHTML = 'Hawaii Royal <p><del>4.2 €</del> 2,8 €</p>';
-    item4.innerHTML = '222';
+    item1.innerHTML = (5*0.94).toFixed(2) + "€";
+    item2.innerHTML = (1*0.94).toFixed(2) + "€";
+    item3.innerHTML = (1.6*0.94).toFixed(2) + "€";
+    item4.innerHTML = (2.4*0.94).toFixed(2) + "€";
+    item5.innerHTML = (3.4*0.94).toFixed(2) + "€";
+    item6.innerHTML = (4*0.94).toFixed(2) + "€";
+    item7.innerHTML = (0.8*0.94).toFixed(2) + "€";
+    item8.innerHTML = (4.6*0.94).toFixed(2) + "€";
+    item9.innerHTML = (4.8*0.94).toFixed(2) + "€";
+    item10.innerHTML = (1.4*0.94).toFixed(2) + "€";
+    item11.innerHTML = (5.4*0.94).toFixed(2) + "€";
+    item12.innerHTML = (1.2*0.94).toFixed(2) + "€";
   } else if (selectedCurrency === "JPY") {
-    item1.innerHTML = 'Double Drive Burger <p><del>1358¥</del> 1154 ¥</p>';
-    item2.innerHTML = 'Chicago Classic <p><del>951 ¥</del> 679 ¥</p>';
-    item3.innerHTML = 'Hawaii Royal <p><del>611 ¥</del> 407 ¥</p>';
-    item4.innerHTML = '222';
+    item1.innerHTML = (5*137.82).toFixed(2) + "¥";
+    item2.innerHTML = (1*137.82).toFixed(2) + "¥";
+    item3.innerHTML = (1.6*137.82).toFixed(2) + "¥";
+    item4.innerHTML = (2.4*137.82).toFixed(2) + "¥";
+    item5.innerHTML = (3.4*137.82).toFixed(2) + "¥";
+    item6.innerHTML = (4*137.82).toFixed(2) + "¥";
+    item7.innerHTML = (0.8*137.82).toFixed(2) + "¥";
+    item8.innerHTML = (4.6*137.82).toFixed(2) + "¥";
+    item9.innerHTML = (4.8*137.82).toFixed(2) + "¥";
+    item10.innerHTML = (1.4*137.82).toFixed(2) + "¥";
+    item11.innerHTML = (5.4*137.82).toFixed(2) + "¥";
+    item12.innerHTML = (1.2*137.82).toFixed(2) + "¥";
   }
 }
 
